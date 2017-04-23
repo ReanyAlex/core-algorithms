e9da101f55d9fab2c83765af3d6a06df7c333ff4
Alex Reany <reanyalex@gmail.com>
2017-04-22 18:07:07 -0700

    fixed one error

--- setUnion.js
+++ setUnion.js
@@ -1,17 +1,14 @@
 //Return the union of two sets.
 
 function setUnion(a,b) {
-  console.log(a);
   // combine arrays and filter out what matches
-  const unionArr = a.concat(b).filter(function(elem, index, arr) {
+  return a.concat(b).filter(function(elem, index, arr) {
     return index === arr.indexOf(elem);
-})
-
-  console.log(unionArr);
+  })
 }
 
 
 const a = [1, 2, 3, 4]
 const b = [2, 4, 6, 8]
-setUnion(a, b)
+console.log(setUnion(a, b));
 // => [1, 2, 3, 4, 6, 8]
